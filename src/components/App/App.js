f459b60a643632a00af97eb0a64fa32ef40f4f02
kyle lee <kyleleeca@gmail.com>
2022-01-24 16:21:16 -0500

    add components and styles

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import React from 'react';
+import BusinessList from '../BusinessList/BusinessList';
+import Business from '../Business/Business';
+import SearchBar from '../SearchBar/SearchBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList />
+  </div>
+  );
+}
+
+export default App;
