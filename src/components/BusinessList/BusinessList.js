70066757f20eae3fb36b2dc5acb2e49d9935c13f
kyle lee <kyleleeca@gmail.com>
2022-01-31 13:23:27 -0500

    add interaction with Yelp API

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,9 +6,10 @@ class BusinessList extends React.Component{
     render() {
         return (
             <div className="BusinessList">
-                {
+                {   
+        
                     this.props.businesses.map(business => {
-                        return <Business business={business}/>
+                        return <Business key={business.id} business={business}/>
                     })
                 }
                 
